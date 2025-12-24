7c0fea062d5dd77b13d380fdef6b46e8fe88b6f8
Michael Mao <215769569+michm1523@users.noreply.github.com>
2025-12-24 11:28:51 -0500

    Add book card styles and read button functionality

---
+++ app.js
@@ -0,0 +1,72 @@
+const libary = [];
+const bookDisplay = document.querySelector(".books");
+
+function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibary(title, author, pages, read) {
+  libary.push(new Book(title, author, pages, read));
+}
+
+function displayBooks() {
+  while (bookDisplay.firstChild) {
+    bookDisplay.firstChild.remove();
+  }
+  for (const book of libary) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    if (book.read) {
+      card.classList.add("read");
+    }
+
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = book.title;
+    card.appendChild(titleElement);
+
+    const authorElement = document.createElement("p");
+    authorElement.textContent = book.author;
+    card.appendChild(authorElement);
+
+    const pagesElement = document.createElement("p");
+    pagesElement.textContent = `Pages: ${book.pages}`;
+    card.appendChild(pagesElement);
+
+    const readIndicator = document.createElement("div");
+    readIndicator.textContent = book.read ? "Read" : "Unread";
+    readIndicator.classList.add("read-indicator");
+    card.appendChild(readIndicator);
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttons");
+
+    const readButton = document.createElement("button");
+    readButton.classList.add("read-button");
+    readButton.setAttribute("type", "button");
+    readButton.textContent = book.read ? "Mark as unread" : "Mark as read";
+    readButton.addEventListener("click", (e) => {
+      book.read = !book.read;
+      displayBooks();
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.textContent = "Delete";
+
+    buttonContainer.appendChild(readButton);
+    buttonContainer.appendChild(deleteButton);
+    card.appendChild(buttonContainer);
+
+    bookDisplay.appendChild(card);
+  }
+}
+
+addBookToLibary("idk", "safds", 1232, true);
+addBookToLibary("idk2", "safdsafds", 210, false);
+
+displayBooks();
